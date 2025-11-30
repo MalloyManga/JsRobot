a0ae4b4482652978bb19d63eb5a2178b79f1e408
Malloy <li3210361050@gmail.com>
2025-11-30 21:41:27 +0800

    修复无法识别css路径的部署bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,26 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  // 开启 Nuxt 4 目录结构
+  future: {
+    compatibilityVersion: 4,
+  },
+
   css: ['./app/assets/css/main.css'],
   ssr: false,
+
+  // 关键配置
   app: {
-    baseURL: '/JsRobot/'
+    baseURL: '/JsRobot/', // 必须和仓库名一致，注意前后斜杠
+    buildAssetsDir: '/_nuxt/',
   },
+
+  // 显式指定 GitHub Pages 预设
+  nitro: {
+    preset: 'github-pages'
+  },
+
   vite: {
     plugins: [
       tailwindcss()
